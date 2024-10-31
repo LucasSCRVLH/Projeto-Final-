47bdaf729176fb3d75d03402efa1c68929e6f0b9
Lucas Carvalho <blujack11@gmail.com>
2024-10-30 21:45:31 -0300

    Envio da estilização da pagina home

--- Laddiwy/src/App.jsx
+++ Laddiwy/src/App.jsx
@@ -4,12 +4,15 @@ import './App.css'
 import { Home } from './Components/Home/home'
 import { Header } from './Components/Header/header'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header/>
+      <Home/>
+    
     </>
   )
 }
--- Laddiwy/src/Components/Header/header.jsx
+++ Laddiwy/src/Components/Header/header.jsx
@@ -3,6 +3,7 @@ import logo from "../../img/logo.png";
 import { MagnifyingGlass } from "@phosphor-icons/react";
 import styles from "./header.module.css";
 
+
 export function Header() {
   return (
     <div className={styles.container}>
@@ -37,57 +38,10 @@ export function Header() {
     </div>
   );
 }
-=======
-
-import "./header.module.css"
-import logo from "../../img/logo.png"
-
-
-export function Header(){
-
-    return(
-
-        <div className="container">
-
-           <div className="imagem">
-
-            <img src={logo} alt="logo" />
-
-           </div>
-           {/* <button><img src={rj} alt="rj" /></button>
-         */}
-           
-        
-        <ul className="list-header">
-           
-                <li><a className="list-header"  href="#">Home</a></li>
-           
-                <li><a className="list-header" href="#">Favoritos</a></li>
-            
-                <li><a className="list-header" href="#">Login</a></li>
-            
-        
-
-            </ul>
-                
-                <div className="search">
-                    <input  type="text" placeholder="Pesquise seu concurso" /> 
-                    <button><img className="lupaLogo" src="https://cdn-icons-png.flaticon.com/512/64/64673.png" alt="lupa" /> </button>
-                </div>
-           
-           
-       
-
-
-        </div>
-
-
-
-
-    );
 
 
 
 
 
-}
+
+
